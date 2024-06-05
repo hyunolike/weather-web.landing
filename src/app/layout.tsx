4a9b60501045ecffbf2f2e56e14a128fc9b37c73
hh.jang <fallear1@gmail.com>
2024-06-05 21:07:42 +0900

    feat: prettier 추가 (#4)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import {Providers} from "@/app/providers";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,6 +12,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
       <html lang="en" className='dark'>
         <body>
