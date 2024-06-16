3da258a6cdc836f740bbb8d210c415dca6a3d0c0
hh.jang <fallear1@gmail.com>
2024-06-16 19:25:17 +0900

    chore: paths 옵션 사용에 따른 임포트 경로 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 import { Providers } from './_providers/providers'
 import RootLayoutComponent from './_components/RootLayoutComponent'
-import { MSWComponent } from './_components/MSWComponent'
+import { MSWComponent } from '@/app/_components/MSWComponent'
 
 export const metadata: Metadata = {
   title: '날씨의 속삭임',
