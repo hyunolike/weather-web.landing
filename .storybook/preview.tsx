3f6a18f75d0a6b3523802ba3805d500abcca68f4
hh.jang <fallear1@gmail.com>
2024-06-18 23:29:51 +0900

    feat: 버튼 UI 테스트 추가 (#16 #11)

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,6 +1,7 @@
 import type { Preview } from '@storybook/react'
 import '@/app/globals.css' // Import global styles (NextUI 적용 필수값)
 // NextUI는 tailwindcss를 기반으로 하기 때문에, tailwindcss의 기본값을 불러와야 한다.
+import './style.scss'
 
 const preview: Preview = {
   parameters: {
--- src/app/(default)/_component/ServiceModalButton/ServiceModalButton.stories.tsx
+++ src/app/(default)/_component/ServiceModalButton/ServiceModalButton.stories.tsx
@@ -1,6 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import ServiceModalButton from './index';
-import { NextUIProvider } from '@nextui-org/react';
 
 const meta: Meta<typeof ServiceModalButton> = {
   title: 'ServiceModalButton',
@@ -11,13 +10,35 @@ export default meta;
 
 type Story = StoryObj<typeof ServiceModalButton>;
 
-export const Default: Story = {
+export const 입장하기: Story = {
   args: {
-    serviceName: "Button",
+    serviceName: "날씨의속삭임 입장하기 (정회원)",
+    isDisabled: false,
+    isBadge: true,
+    title: '날씨의 속삭임 서비스 요약',
+    content: '날씨의 속삭임은 "날씨 변화에 따른 다양한 감정과 상황을 익명으로 소통하며 공감대를 형성할 수 있는 커뮤니티"입니다.\n\n\n 회원가입을 통해 날씨의 속삭임을 경험해보세요!',
+    url: '#',
+  },
+};
+
+export const 체험하기 : Story = {
+  args: {
+    serviceName: "날씨의속삭임 체험하기 (비회원)",
+    isDisabled: false,
+    isBadge: true,
+    title: '날씨의 속삭임 서비스 요약',
+    content: '날씨의 속삭임은 "날씨 변화에 따른 다양한 감정과 상황을 익명으로 소통하며 공감대를 형성할 수 있는 커뮤니티"입니다.\n\n\n 회원가입을 통해 날씨의 속삭임을 경험해보세요!',
+    url: '#',
+  },
+};
+
+export const 체험하기_비활성화 : Story = {
+  args: {
+    serviceName: "날씨의속삭임 체험하기 (비회원)",
     isDisabled: true,
     isBadge: true,
-    title: 'Button',
-    content: 'Button',
-    url: 'Button',
+    title: '날씨의 속삭임 서비스 요약',
+    content: '날씨의 속삭임은 "날씨 변화에 따른 다양한 감정과 상황을 익명으로 소통하며 공감대를 형성할 수 있는 커뮤니티"입니다.\n\n\n 회원가입을 통해 날씨의 속삭임을 경험해보세요!',
+    url: '#',
   },
 };
